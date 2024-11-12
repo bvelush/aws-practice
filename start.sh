e0068a79c03b7ceb7909f0091e39e61e5b35e133
Dan Velush <bvelush@hotmail.com>
2024-11-12 06:15:35 -0800

    adding config files

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
+sudo apt update
 sudo apt install python3-virtualenv -y
+sudo apt install python3 python3-pip python3-virtualenv nginx curl
 
 git clone https://github.com/bvelush/aws-practice.git apiv1/
 cd apiv1
